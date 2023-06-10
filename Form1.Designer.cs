2213d4324a7c682327ccd8edb406946ec8abf68f
Matheus Eickhoff <127603510+Matheus-Ei@users.noreply.github.com>
2023-06-10 18:42:24 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Aplicativo_de_Gestão_de_Finanças_Pessoais
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerenciarReceitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.despesasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerenciarDespesasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oatrimonioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerenciarMeusPatrimoniosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.ForestGreen;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.mainToolStripMenuItem,
+            this.despesasToolStripMenuItem,
+            this.oatrimonioToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 15, 10, 2);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(140, 1041);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuLateral";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuLateralClicked);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(120, 23);
+            this.helpToolStripMenuItem.Text = "Visão Geral";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerenciarReceitasToolStripMenuItem});
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(93, 23);
+            this.mainToolStripMenuItem.Text = "Receitas";
+            // 
+            // gerenciarReceitasToolStripMenuItem
+            // 
+            this.gerenciarReceitasToolStripMenuItem.Name = "gerenciarReceitasToolStripMenuItem";
+            this.gerenciarReceitasToolStripMenuItem.Size = new System.Drawing.Size(240, 24);
+            this.gerenciarReceitasToolStripMenuItem.Text = "Gerenciar Receitas";
+            // 
+            // despesasToolStripMenuItem
+            // 
+            this.despesasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerenciarDespesasToolStripMenuItem});
+            this.despesasToolStripMenuItem.Name = "despesasToolStripMenuItem";
+            this.despesasToolStripMenuItem.Size = new System.Drawing.Size(93, 23);
+            this.despesasToolStripMenuItem.Text = "Despesas";
+            // 
+            // gerenciarDespesasToolStripMenuItem
+            // 
+            this.gerenciarDespesasToolStripMenuItem.Name = "gerenciarDespesasToolStripMenuItem";
+            this.gerenciarDespesasToolStripMenuItem.Size = new System.Drawing.Size(240, 24);
+            this.gerenciarDespesasToolStripMenuItem.Text = "Gerenciar Despesas";
+            // 
+            // oatrimonioToolStripMenuItem
+            // 
+            this.oatrimonioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerenciarMeusPatrimoniosToolStripMenuItem});
+            this.oatrimonioToolStripMenuItem.Name = "oatrimonioToolStripMenuItem";
+            this.oatrimonioToolStripMenuItem.Size = new System.Drawing.Size(120, 23);
+            this.oatrimonioToolStripMenuItem.Text = "Patrimonios";
+            // 
+            // gerenciarMeusPatrimoniosToolStripMenuItem
+            // 
+            this.gerenciarMeusPatrimoniosToolStripMenuItem.Name = "gerenciarMeusPatrimoniosToolStripMenuItem";
+            this.gerenciarMeusPatrimoniosToolStripMenuItem.Size = new System.Drawing.Size(312, 24);
+            this.gerenciarMeusPatrimoniosToolStripMenuItem.Text = "Gerenciar meus Patrimonios";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(863, 308);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red};
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(284, 193);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "Despesas Mensais";
+            this.chart1.Titles.Add(title1);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(863, 84);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(284, 193);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Title1";
+            title2.Text = "Receitas Mensais";
+            this.chart2.Titles.Add(title2);
+            // 
+            // chart4
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea3);
+            this.chart4.Cursor = System.Windows.Forms.Cursors.Default;
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart4.Legends.Add(legend3);
+            this.chart4.Location = new System.Drawing.Point(211, 99);
+            this.chart4.Name = "chart4";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart4.Series.Add(series3);
+            this.chart4.Size = new System.Drawing.Size(535, 378);
+            this.chart4.TabIndex = 1;
+            this.chart4.Text = "chart1";
+            title3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title3.Name = "Title1";
+            title3.Text = "Despesas X Receitas";
+            this.chart4.Titles.Add(title3);
+            // 
+            // chart3
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart3.Legends.Add(legend4);
+            this.chart3.Location = new System.Drawing.Point(1210, 213);
+            this.chart3.Name = "chart3";
+            this.chart3.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            this.chart3.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))))};
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart3.Series.Add(series4);
+            this.chart3.Size = new System.Drawing.Size(284, 193);
+            this.chart3.TabIndex = 1;
+            this.chart3.Text = "chart1";
+            title4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title4.Name = "Title1";
+            title4.Text = "Patrimonio";
+            this.chart3.Titles.Add(title4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem despesasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerenciarReceitasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerenciarDespesasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oatrimonioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerenciarMeusPatrimoniosToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicativo_de_Gestão_de_Finanças_Pessoais
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configurar a conexão com o banco de dados
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\t4iga\OneDrive\Empresa\Versões do Aplicativo\Aplicativo de Gestão de Finanças Pessoais\database.accdb;";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            // Consulta SQL para obter os dados do banco de dados
+            string query = "SELECT valor, titulo, data FROM receitas";
+
+            // Criação do comando SQL
+            OleDbCommand command = new OleDbCommand(query, connection);
+
+            // Abertura da conexão
+            connection.Open();
+
+            // Leitura dos dados do banco de dados
+            OleDbDataReader reader = command.ExecuteReader();
+
+
+
+            // Adicionar alguns pontos de dados ao gráfico
+            chart4.Series.Clear();
+            chart4.Series.Add("Dados");
+            chart4.Series["Dados"].Points.AddXY("Categoria 1", 10);
+            chart4.Series["Dados"].Points.AddXY("Categoria 2", 20);
+            chart4.Series["Dados"].Points.AddXY("Categoria 3", 15);
+            // Configurar o tipo de gráfico
+            chart4.Series["Dados"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+
+            // Limpar os pontos de dados existentes no gráfico
+            chart1.Series["dadosReceita"].Points.Clear();
+            // Adicionar os pontos de dados do banco de dados ao gráfico
+            while (reader.Read())
+            {
+                string categoria = reader["titulo"].ToString();
+                int valor = Convert.ToInt32(reader["valor"]);
+
+                chart1.Series["dadosReceita"].Points.AddXY(categoria, valor);
+            }
+            // Configurar o tipo de gráfico
+            chart2.Series["dadosReceita"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+
+            // Adicionar alguns pontos de dados ao gráfico
+            chart1.Series.Clear();
+            chart1.Series.Add("dadosDespesas");
+            chart1.Series["dadosDespesas"].Points.AddXY("Categoria 1", 10);
+            chart1.Series["dadosDespesas"].Points.AddXY("Categoria 2", 20);
+            chart1.Series["dadosDespesas"].Points.AddXY("Categoria 3", 15);
+            // Configurar o tipo de gráfico
+            chart1.Series["dadosDespesas"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+
+            // Adicionar alguns pontos de dados ao gráfico
+            chart3.Series.Clear();
+            chart3.Series.Add("dadosPatrimonio");
+            chart3.Series["dadosPatrimonio"].Points.AddXY("Categoria 1", 10);
+            chart3.Series["dadosPatrimonio"].Points.AddXY("Categoria 2", 20);
+            chart3.Series["dadosPatrimonio"].Points.AddXY("Categoria 3", 15);
+            // Configurar o tipo de gráfico
+            chart3.Series["dadosPatrimonio"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+        }
+
+        private void menuLateralClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
